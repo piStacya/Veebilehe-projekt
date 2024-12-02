9a0c76072e876adf9fe71d757c466306026deba9
piStacya <anastassia.kaarmann@gmail.com>
2024-12-02 18:40:15 +0200

    uuendused...

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-const tips = [
-    "Anna oma koerale piisavalt vett ja sööki iga päev.",
-    "Harja oma kassi karv vähemalt kord nädalas.",
-    "Paku oma jänesele alati värsket vett ja porgandeid.",
-    "Loomad vajavad liikumist, mängi nendega regulaarselt.",
-    "Külastage loomaarsti vähemalt kord aastas.",
-    "Veendu, et su koduloomal oleks mugav ja soe magamiskoht.",
-    "Kasutage mänguasju, et stimuleerida oma lemmiklooma aju ja aktiivsust."
-  ];
-  
-  const tipButton = document.getElementById("generate-tip");
-  const dailyTip = document.getElementById("daily-tip");
-  
-  tipButton.addEventListener("click", function() {
+document.getElementById("generate-tip").addEventListener("click", function () {
+    const tips = [
+      "Anna oma lemmikloomale regulaarselt vett!",
+      "Ära unusta lemmiklooma küüsi lõigata.",
+      "Käi lemmikloomaga regulaarselt kontrollis.",
+      "Paku oma lemmikloomale mänguasju ja treeningut.",
+      "Tagage lemmikloomale rahulik magamiskoht."
+    ];
+    
     const randomTip = tips[Math.floor(Math.random() * tips.length)];
-    dailyTip.textContent = `Päeva näpunäide: ${randomTip}`;
+    document.getElementById("daily-tip").innerText = randomTip;
   });
+  
